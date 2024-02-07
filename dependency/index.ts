9f5f5e7bdbee0f206cf9dce745b4241782d8d054
Gal Amouyal <galamouy@Gals-MacBook-Pro.local>
2024-02-07 08:56:48 +0200

    a

--- dependency/index.ts
+++ dependency/index.ts
@@ -1,8 +1,11 @@
-function f<T extends string | number>(a: T, b: T) {
+function f<
+  T extends string | number,
+  R extends T extends string ? string : number
+>(a: T, b: T): R {
   if (typeof a === "string") {
-    return a + ":" + b;
+    return (a + ":" + b) as R;
   } else {
-    return (a as number) + (b as number); // assertion
+    return ((a as number) + (b as number)) as R;
   }
 }
 
@@ -14,5 +17,20 @@ function f2<
     return (a + ":" + b) as R;
   } else {
     return ((a as number) + (b as number)) as R;
+  }
+}
+
+// function Constraint(a: number | string, b: number | string) {
+//   if (typeof a === "string") {
+//     return a + ":" + b;
+//   } else {
+//     return a + b;
+//   }
+// }
+function Constraint<T extends string | number>(a: T, b: T) {
+  if (typeof a === "string") {
+    return a + ":" + b;
+  } else {
+    return a + b;
   }
 }
